f14b82d9396a4ed691dc2ad694b4ae979cb52a64
Girish <girishkumar518@gmail.com>
2021-07-01 17:24:43 +0100

    Project created and initial commit

---
+++ src/main/java/com/cards/app/CardsAppApplication.java
@@ -0,0 +1,13 @@
+package com.cards.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cards/app/CardsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cards.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
