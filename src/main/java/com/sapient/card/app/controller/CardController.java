c2c041373e2ac7d318446e0bbc8a6df54ca5e823
Girish <girishkumar518@gmail.com>
2021-07-04 22:01:22 +0100

    ui fix

--- src/main/java/com/sapient/card/app/controller/CardController.java
+++ src/main/java/com/sapient/card/app/controller/CardController.java
@@ -9,15 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.xml.bind.ValidationException;
-
 @RestController
+@CrossOrigin
 @Slf4j
 public class CardController {
 
